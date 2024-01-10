18f41fbe82b8a00733f8f0f07199b5c05ab8463d
jogiwa <jogiwa20022@gmx.de>
2024-01-10 14:47:19 +0100

    Import from Spring Initializr

---
+++ src/main/java/de/thowl/bcryptdemo/BcrypTdemoApplication.java
@@ -0,0 +1,13 @@
+package de.thowl.bcryptdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcrypTdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcrypTdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/thowl/bcryptdemo/BcrypTdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thowl.bcryptdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcrypTdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
