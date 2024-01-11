535681e9f7c9335758130eb0610915775b108210
j0giwa <jonasschwind20022@gmx.de>
2024-01-11 20:16:09 +0100

    Halfassed ui update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
 INSERT INTO
     users(id, username, email, password)
 VALUES
-    (1, 'Rüdiger', 'Rüdiger@thowl.de', '$2a$15$q/VCgMfG.XLG55DT2dgehOVFcAMgoV2iwgB7AA2yhzpk53E6uJ.La');
+    (1, 'Rüdiger', 'ruediger@thowl.de', '$2a$15$q/VCgMfG.XLG55DT2dgehOVFcAMgoV2iwgB7AA2yhzpk53E6uJ.La');
